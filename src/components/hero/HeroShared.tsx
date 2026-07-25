127e0aeb53d6db9469fa2262cd9c38b9342d2a59
Prajval Injar <injarprajval@gmail.com>
2026-07-25 17:40:08 +0530

    Lock DesktopHero completely and establish isolated MobileHero component architecture

---
+++ src/components/hero/HeroShared.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export const HERO_ROLES = [
+  "Data Analyst",
+  "Full Stack Developer",
+  "AI Enthusiast",
+  "Problem Solver",
+  "Building Intelligent Products"
+] as const;
+
+export interface MousePosition {
+  x: number;
+  y: number;
+}
